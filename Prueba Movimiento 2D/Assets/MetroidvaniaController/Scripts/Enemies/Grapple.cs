d9608182cbb1ed47fe3623b6f255d04c3b6626c8
Arias002 <arias.miguelUEM@gmail.com>
2022-05-03 16:01:48 +0200

    Revert "a ver, señores ya no hay gancho, pero esta en proceso de estar, mañana christian y yo le preguntraremos a sergio a ver si nos puede ayudar"
    This reverts commit 8f1f776d6ab0d375bcb752cfcac7315c97ff0216.

--- Prueba Movimiento 2D/Assets/MetroidvaniaController/Scripts/Enemies/Grapple.cs
+++ Prueba Movimiento 2D/Assets/MetroidvaniaController/Scripts/Enemies/Grapple.cs
@@ -1,71 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
-	[SerializeField] float pullSpeed = 0.5f;
-	[SerializeField] float stopDistance = 4f;
-	[SerializeField] GameObject hookPrefab;
-	[SerializeField] Transform shootTrransform;
+	public Vector3 direction;
+	public bool hasHit = false;
+	public float speed = 20f;
+	public bool ganchito;
+	public CharacterControllerNonUnity characterController;
 
-	Hook hook;
-	bool pulling;
-	Rigidbody m_Rigidbody;
-	
-
-// Start is called before the first frame update
+	// Start is called before the first frame update
 	void Start()
 	{
-		m_Rigidbody = GetComponent<Rigidbody>();
-		pulling = false;
-		
+		characterController = GameObject.Find("DrawCharacter").GetComponent<CharacterControllerNonUnity>();
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
-		if(hook== null && Input.GetKeyDown(KeyCode.V) || Input.GetKeyDown(KeyCode.K) || Input.GetMouseButtonDown(1) )
-		{
-			StopAllCoroutines();
-			pulling = false;
-			hook = Instantiate(hookPrefab, shootTrransform.position, quaternion.identity).GetComponent<Hook>();
-			hook.Initialize(this, shootTrransform);
-			StartCoroutine(DestroyHookAfterLifetime());
-		}
-
-		if (!pulling || hook == null) return;
-
-		if (Vector3.Distance(transform.position, hook.transform.position) <= stopDistance)
-		{
-			DestroyHook();
-		}
-		else
-		{
-			m_Rigidbody.AddForce((hook.transform.position - transform.position).normalized * pullSpeed, ForceMode.VelocityChange);
+		if (!hasHit)
+        {
+			GetComponent<Rigidbody>().velocity = direction * speed;
 		}
 	}
 
-	public void StartPull()
+	void OnCollisionEnter(Collision collision)
 	{
-		pulling = true;
+		if (collision.gameObject.tag == "Enemy")
+		{
+			characterController.ApplyGrapleForce();
+			Destroy(gameObject);
+		}
+		else if (collision.gameObject.layer == 3)
+		{
+			Destroy(gameObject);
+		}
 	}
-
-	private void DestroyHook()
-	{
-		if (hook == null) return;
-
-		pulling = false;
-		Destroy(hook.gameObject);
-		hook = null;
-
-	}
-
-	private IEnumerator DestroyHookAfterLifetime()
-	{
-		yield return new WaitForSeconds(8f);
-		DestroyHook();
-	}
-
 }
